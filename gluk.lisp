90fc8fc23e8315459728aa9cfaadc642b300a7b8
void777 <nuke.m@bk.ru>
2013-06-26 12:49:59 +0300

    xD

--- gluk.lisp
+++ gluk.lisp
@@ -1,4 +1,32 @@
-(defun hello-world ()
-       (format t "ЫыыыыАаЫ")
-       (print "hello wotld")
+(defun main (arg1 arg2 arg3 arg4) ;#главная функция, аля int main() 
+)
+;;;
+(defun select-filetype () ;#функ-ция распознования типа файла
+)
+;;; 
+(defun open-doc () ;#функция для открытия .doc файлов
+)
+;;;
+(defun open-docx () ;#функция для открытия .docx файлов
+)
+;;;
+(defun open-txt () ;#функция для открытия .txt файлов
+)
+;;;
+(defun split-to-blocks () ;#функция для разбивки текста на блоки из предложений
+)
+;;;
+(defun get-sentence () ;#функция для захвата предложения
+)
+;;;
+(defun get-word () ;#функция для захвата слова
+)
+;;;
+(defun compare-word () ;#подбираем синоним
+)
+;;;
+(defun export-to-doc () ;#функция экспортирования в .doc файлы
+)
+;;;
+(defun _export-to-txt () ;#функция экспорта в .txt
 )
